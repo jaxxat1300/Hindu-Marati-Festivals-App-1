0f9876d341ddba6cd8f7f9948d96c74e015a68d5
jaxxat1300 <163923112+jaxxat1300@users.noreply.github.com>
2025-10-17 12:31:58 -0700

    Move files to root for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
+    unoptimized: true
   },
   output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  trailingSlash: true
 }
 
 module.exports = nextConfig
